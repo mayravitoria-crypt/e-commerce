0bc2b2248a06bd990f88aa851e1b24939eab4aba
Nayra Gama <nayrafgama263@gmail.com>
2026-07-06 17:32:20 -0300

    feat(produto): adiciona componente Produto e remove template padrão do Angular CLI

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+  //!import { RouterOutlet } from '@angular/router';// Remove importação do RouterOutlet,pois não esta sendo utilizado no momento//
+import { Produto } from './components/produto/produto'; //Importa  o componente 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Produto],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
