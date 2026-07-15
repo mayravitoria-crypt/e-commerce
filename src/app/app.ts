e0b5a8975833203d7c0ae5ee3ad928c54a8b8c2f
Mayra Rocha <mayravitoriasiva@gmail.com>
2026-07-15 17:31:08 -0300

    feat (pipe): adicionar pipe para formatação de preço

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component, signal } from '@angular/core';
   //!import { RouterOutlet } from '@angular/router';// Remove importação do RouterOutlet,pois não esta sendo utilizado no momento//
 import { Produto } from './features/produtos/produto/produto'; //Importa  o componente 
-import { ListaProdutos } from './features/produtos/produto/lista-produtos/lista-produtos';
+import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
 @Component({
   selector: 'app-root',
   imports: [ListaProdutos],
