00ecd118a3b9c0b8da768c0e4a91e2196d5ef26c
Mayra Rocha <mayravitoriasiva@gmail.com>
2026-07-15 13:53:12 -0300

    feat:adicionar componente lista-produto

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
   //!import { RouterOutlet } from '@angular/router';// Remove importação do RouterOutlet,pois não esta sendo utilizado no momento//
 import { Produto } from './components/produto/produto'; //Importa  o componente 
+import { ListaProdutos } from './components/lista-produtos/lista-produtos';
 @Component({
   selector: 'app-root',
-  imports: [Produto],
+  imports: [ListaProdutos],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
