fe8de7fed07ab01b1fb6c48d2b1479e8f42c7134
Mayra Rocha <mayravitoriasiva@gmail.com>
2026-07-16 17:09:19 -0300

    feat:adicionar funcionalidade de adicionar e substituir produtos

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -1,14 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { Produto } from '../produto/produto';
-
+import { computed } from '@angular/core';
+import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
 @Component({
   selector: 'app-lista-produtos',
-  imports: [Produto],
+  imports: [Produto, PrecoFormatadoPipe],
   templateUrl: './lista-produtos.html',
   styleUrl: './lista-produtos.css',
 })
 export class ListaProdutos {
-  produto = signal([
+  produtos = signal([
     {nome:'Teclado Gamer', preco:149.00},
     {nome:'Mouse Gamer', preco:299.99},
     {nome:'Monitor Gamer',preco:1599.99},
@@ -19,8 +20,21 @@ export class ListaProdutos {
     console.log('Produto Selecionado: ',nome);
   }
  adicionarProduto(){
-   this.produto.update(listaAtual => [
-    ...listaAtual, {nome:'Sony PlayStation 5', preco: 10000.00}
+   this.produtos.update(listaAtual => [
+    ...listaAtual, 
+    {nome:'Sony PlayStation 5', preco: 10000.00}
+    
    ]);
  }  
+ totalProdutos = computed(() => this.produtos().length);
+
+ valorTotal  = computed(() =>{return this.produtos().reduce
+  ((total, item)=> total + item.preco,0)});
+
+  substituirProdutos(){
+    this.produtos.set([
+      {nome:'Arroz Fazenda', preco: 400},
+      
+    ]);
+}
 }
