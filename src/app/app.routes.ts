46c55d148443b566b64e2b1ee0c34d262f58095c
Mayra Rocha <mayravitoriasiva@gmail.com>
2026-07-22 16:13:51 -0300

    feat(routes): implementa lazy loading e proteção de rotas com authGuard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,25 +1,61 @@
-import { Routes } from '@angular/router';
-import { ListaProdutos} from './features/produtos/lista-produtos/lista-produtos'; 
-import { Home } from './features/home/home/home';
-import { Carrinho } from './features/carrinho/carrinho';
+//import { Routes } from '@angular/router';
+//import { ListaProdutos} from './features/produtos/lista-produtos/lista-produtos'; 
+//import { Home } from './features/home/home/home';
+//import { Carrinho } from './features/carrinho/carrinho';
 
 
-export const routes: Routes = [
-{
-    path: '',
-    component:Home,
-},
-{
-    path:'produtos',
-    component: ListaProdutos,
-},
+//export const routes: Routes = [
+//{
+//    path: '',
+//    component:Home,
+//},
+// //código logado router
+//{
+//   //  path:'produtos',
+//    //component: ListaProdutos,
+//    ///
+//
 
-{
-   path:'Carrinho',
-   componet: Carrinho,
+//{
+//   path:'produtos',
+  // loadComponent: () =>
+ //    import ('./features/produtos/lista-produtos/lista-produtos').then(m => m.ListaProdutos)
 
-},
+//},
+  //{
+  //   path:'carrinho,' 
+  //    component: Carrinho,
+ // }
+//];
 
-];
+
+//! importações
+import { Routes } from "@angular/router";
+import { authGuard } from "./core/auth.guard";
+
+export const  routes: Routes =[
+    {
+        path:'',
+        loadComponent: () =>
+            import ('./features/home/home/home') .then((m)=> m.Home),
+      },
+      {
+        path:'carrinho',
+        canActivate:[authGuard],
+        loadComponent:()=>
+            import ('./features/carrinho/carrinho').then((m)=>m.Carrinho),
+    },
+     {
+        path:'produtos',
+        loadComponent: () =>
+            import('./features/produtos/lista-produtos/lista-produtos').then((m)=> m.ListaProdutos),
+
+      },
+      {
+        path:'**',
+        redirectTo: '',
+      },
+    ];
+
 
 
